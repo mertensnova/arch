71a695fc4fbc3804c0cca6ca8a2ca230e8ebfa73
mertensnova <amr8644@gmail.com>
2024-11-13 20:42:50 +0300

    updated script

--- scripts/arch.sh
+++ scripts/arch.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 sudo pacman -Syu
+sudo pacman -S dosfstools mtools os-prober base-devel autoconf git
 sudo pacman -S wget pulseaudio unzip resolvconf pavucontrol
-#sudo pacman -S thunar-volman gvfs gvfs-afc
+sudo pacman -S nautilus gvfs gvfs-afc
+sudo pacman -S mtpfs libmtp gvfs-mtp
 
 echo "Installing Dev enviroment" | cowsay 
-sudo pacman -S tmux neovim neofetch lightdm
-git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
-git clone --depth 1 https://github.com/wbthomason/packer.nvim\ ~/.local/share/nvim/site/pack/packer/start/packer.nvim
+sudo pacman -S tmux neovim fastfetch ly
+
+git clone --depth 1 https://github.com/wbthomason/packer.nvim\
+ ~/.local/share/nvim/site/pack/packer/start/packer.nvim
 
 echo "Installing libs" | cowsay 
-sudo pacman -S base-devel autoconf git
 cd
 cd Downloads
 git clone https://aur.archlinux.org/yay-git.git
@@ -22,11 +24,10 @@ echo "Installing programming languages" | cowsay
 sudo pacman -S go gcc cmake make
 
 echo "Installing utils" | cowsay 
-sudo pacman -S ncdu nginx-mainline docker ufw openvpn btop fzf
+sudo pacman -S ncdu nginx-mainline ufw openvpn btop fzf
 yay -S  wireguard-arch wireguard-tools
 
-
 sudo pacman -S kitty
-sudo pacman -S wofi waybar hyperland  hyprpaper hyprlang hyprshot
+sudo pacman -S wofi waybar hyperland hyprpaper hyprshot
 
-sudo systemctl enable --now  lightdm
+sudo systemctl enable --now ly
